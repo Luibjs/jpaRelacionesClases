8b288ab7542fc2f05526bb5c47bb32d7fb45a54a
Luibjs <luissantiagobeltranjimenez70@gmail.com>
2022-11-24 13:29:16 -0500

    iniciando el tema de relaciones entre tablas y mapeo de clases

---
+++ src/main/java/com/mensajes/jpaprueba/JpaPrueba.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mensajes.jpaprueba;
+
+/**
+ *
+ * @author santi
+ */
+public class JpaPrueba {
+
+    public static void main(String[] args) {
+        //orm realiza mapeas entre clases y tablas de la BD
+        
+        
+    }
+}
